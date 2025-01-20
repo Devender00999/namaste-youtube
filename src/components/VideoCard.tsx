8a2a40cf2bf529fdcc8905843f6eccb4ebbebce5
Devender Kumar <devender@rubick.ai>
2025-01-20 11:09:45 +0530

    Added Video Card

--- src/components/VideoCard.tsx
+++ src/components/VideoCard.tsx
@@ -1,8 +1,27 @@
 import YouTubeVideo from "../utils/types/YTVideo";
+import dayjs from "dayjs";
+import relativeTime from "dayjs/plugin/relativeTime";
+dayjs.extend(relativeTime);
 
 const VideoCard = ({ info }: { info: YouTubeVideo }) => {
    console.log({ info });
-   return <div>{info?.snippet?.title}</div>;
+   return (
+      <div className="flex flex-col">
+         <div className="">
+            <img
+               className="rounded-xl"
+               src={info?.snippet?.thumbnails?.high?.url}
+            />
+         </div>
+         <div className="">
+            <div>{info?.snippet?.title}</div>
+            <div className="flex gap-2">
+               <span>{info.statistics.viewCount}</span>
+               {dayjs(info?.snippet?.publishedAt).fromNow()}
+            </div>
+         </div>
+      </div>
+   );
 };
 
 export default VideoCard;
